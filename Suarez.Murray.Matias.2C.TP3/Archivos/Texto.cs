ac6b9c4027076b12992f31d6c2b2ad52ec4dac06
murraymatias <murray.matias@gmail.com>
2019-11-09 05:30:35 -0300

    Agregada clase xml

---
+++ Suarez.Murray.Matias.2C.TP3/Archivos/Texto.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Excepciones;
+
+namespace Archivos
+{
+    public class Texto : IArchivo<string>
+    {
+        public bool Guardar(string archivo,string datos)
+        {
+            try
+            {
+                using(StreamWriter writer = new StreamWriter(archivo))
+                {
+                    writer.WriteLine(datos);
+                }
+                return true;
+            }
+            catch(Exception exception)
+            {
+                throw new ArchivosException(exception);
+            }
+        }
+
+        public bool Leer(string archivo,out string datos)
+        {
+            try
+            {
+                using(StreamReader reader = new StreamReader(archivo))
+                {
+
+                }
+                return true;
+            }
+            catch(Exception exception)
+            {
+                throw new ArchivosException(exception);
+            }
+        }
+    }
+}
---
+++ Suarez.Murray.Matias.2C.TP3/Archivos/Xml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Excepciones;
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Archivos
+{
+    public class Xml<T> : IArchivo<T>
+    {
+        public bool Guardar(string archivo,T datos)
+        {
+            try
+            {
+                using (XmlTextWriter writer = new XmlTextWriter(archivo, Encoding.UTF8))
+                {
+                    XmlSerializer serializer = new XmlSerializer(typeof(T));
+                    serializer.Serialize(writer, datos);                   
+                }
+                return true;
+            }
+            catch(Exception exception)
+            {
+                throw new ArchivosException(exception);
+            }
+        }
+
+        public bool Leer(string archivo,out T datos)
+        {
+            try
+            {
+                using (XmlTextReader reader = new XmlTextReader(archivo))
+                {
+                    XmlSerializer serializer = new XmlSerializer(typeof(T));
+                    datos = (T)serializer.Deserialize(reader);
+                }
+                return true;
+            }
+            catch(Exception exception)
+            {
+                throw new ArchivosException(exception);
+            }
+        }
+    }
+}
